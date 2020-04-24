6bb48aa08da9fb08779ae040923aec46aa00d1d7
Ponce Streaming <46233347+19971738@users.noreply.github.com>
2020-04-24 17:47:12 -0500

    Add files via upload

---
+++ Data types, 1.py
@@ -0,0 +1,13 @@
+###Single quotes for short texts,doble quotes for big texts to increase readibility
+a = 5
+print(a, "is of type", type(a))
+
+a = 2.0
+print(a, "is of type", type(a))
+
+b = [4, 'b', 89, 'Hallo']
+print(b, " is of type", type(b))
+
+a = 1+6j
+print(a, "is complex number?", isinstance(a,complex))
+
