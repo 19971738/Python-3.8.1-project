5870f2064e71f1cc6988b2151b16f905e308d83d
Ponce Streaming <46233347+19971738@users.noreply.github.com>
2020-04-24 17:23:46 -0500

    Add files via upload

---
+++ Area of a circle, 3.py
@@ -0,0 +1,3 @@
+from math import pi # Acceess to the constant
+r = float(input ("Input the radius of the circle : "))
+print ("The area of the circle with radius " + str(r) + " is: " + str(pi * r**2))
