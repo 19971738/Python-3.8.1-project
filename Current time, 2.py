27590ed0f685f121b9580739e7caffdafc3062f9
Ponce Streaming <46233347+19971738@users.noreply.github.com>
2020-04-24 16:41:02 -0500

    Add files via upload
    This program access to the current time from the computer

---
+++ Current time, 2.py
@@ -0,0 +1,7 @@
+
+import datetime #library to access to data.
+import time
+now = datetime.datetime.now() 
+print ("Current date and time : ")
+print (now.strftime("%Y-%m-%d %H:%M:%S")) #important strftime to split the data to the user
+
